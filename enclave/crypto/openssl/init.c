e98d49496a6b6da46b08d4cf5c28baa5f4ec112a
Danny Carpenter <dcarp@microsoft.com>
2023-04-27 00:27:17 +0000

    successfully building openssl_3 oecrypto lib
    Signed-off-by: Danny Carpenter <dcarp@microsoft.com>

--- enclave/crypto/openssl/init.c
+++ enclave/crypto/openssl/init.c
@@ -73,8 +73,59 @@ done:
     return (result == OE_SYMCRYPT_ENGINE_SUCCESS) ? 1 : 0;
 }
 
+#if OECRYPTO_OPENSSL_VER < 3
+static ENGINE* _rdrand_engine;
+
+static void _finalize(void)
+{
+    if (_rdrand_engine)
+    {
+        ENGINE_finish(_rdrand_engine);
+        ENGINE_free(_rdrand_engine);
+        ENGINE_cleanup();
+        _rdrand_engine = NULL;
+    }
+}
+
+static void _initialize_rdrand_engine()
+{
+    int result = 0;
+
+    /* Initialize rdrand engine. */
+    ENGINE_load_rdrand();
+    _rdrand_engine = ENGINE_by_id("rdrand");
+    if (_rdrand_engine == NULL)
+        goto done;
+
+    result = ENGINE_init(_rdrand_engine);
+    if (result == 0)
+        goto done;
+
+    result = ENGINE_set_default(_rdrand_engine, ENGINE_METHOD_RAND);
+    if (result == 0)
+        goto done;
+
+    if (!atexit(_finalize))
+        goto done;
+
+    result = 1;
+
+done:
+    if (result == 0)
+    {
+        OE_TRACE_ERROR("OpenSSL initialization failed");
+        _finalize();
+    }
+    return;
+}
+#endif // OECRYPTO_OPENSSL_VER < 3
+
 static void _initialize(void)
 {
+#if OECRYPTO_OPENSSL_VER >= 3
+    OPENSSL_init_crypto(OPENSSL_INIT_NO_LOAD_CONFIG, NULL);
+#endif
+
     /* _initialize_symcrypt_engine only registers the SymCrypt engine and
      * returns 1 if the enclave opts into the engine at link-time. Otherwise,
      * the weak implementation of the function is used, which always returns 0.
@@ -95,6 +146,12 @@ static void _initialize(void)
          * included properly */
         oe_sgx_get_additional_host_entropy(data, HOST_ENTROPY_TEST_SIZE);
     }
+#if OECRYPTO_OPENSSL_VER < 3
+    else
+    {
+        _initialize_rdrand_engine();
+    }
+#endif // OECRYPTO_OPENSSL_VER < 3
 }
 
 void oe_crypto_initialize(void)
