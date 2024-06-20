94593020f846d19f7d347f255f1bbbac18cd828b
Luong-Minh Nguyen <nguyen.luongminh297@gmail.com>
2024-06-20 21:38:53 +0000

    Fixed bug that cause OSSL_provider_init to not be called
    Signed-off-by: Luong-Minh Nguyen <nguyen.luongminh297@gmail.com>

--- enclave/crypto/openssl/symcrypt_provider.c
+++ enclave/crypto/openssl/symcrypt_provider.c
@@ -1,10 +1,8 @@
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
-#if OECRYPTO_OPENSSL_VER >= 3
 #include <openenclave/bits/defs.h>
 #include <openenclave/internal/crypto/init.h>
-#include <openssl/core.h>
 
 /* When the enclave does not opt into the SymCrypt provider at link time,
  * the following implementation (weak) will be picked by the linker.
@@ -12,12 +10,12 @@
  * init.c to prevent the linker pulls in the symbol along with the
  * oe_crypto_initialize function. */
 int _oe_scossl_provider_initialize(
-    const OSSL_CORE_HANDLE* handle,
-    const OSSL_DISPATCH* in,
-    const OSSL_DISPATCH** out,
+    const void* handle,
+    const void* in,
+    const void** out,
     void** provctx)
 {
     return 0;
 }
+
 OE_WEAK_ALIAS(_oe_scossl_provider_initialize, OSSL_provider_init);
-#endif
